bdf28324615a1718e1e543d627700faf58641f47
Sudipta Sen <sanborn.sen@gmail.com>
2019-06-17 20:49:09 +0530

    formatted files following JS code convention

--- src/helper.js
+++ src/helper.js
@@ -5,27 +5,27 @@ var cloneDeep = require('lodash.clonedeep')
  * the options passed into SezzleJS'
  * constructor is compatible with the current
  * SezzleJS version. In other words, this
- * function is used for backwards compatability 
+ * function is used for backwards compatability
  * with older versions
  * @param options options passed into SezzleJS' constructor
  * @return compatible object with current SezzleJS version
  */
-exports.makeCompatible = function(options) {
-  if(typeof (options.configGroups) === 'undefined') {
-		// most likely old config, must wrap it in config group
-		// deep clone to prevent circular structure
-		options = {
-			configGroups: [cloneDeep(options)]
-		};
-	}
+exports.makeCompatible = function (options) {
+  if (typeof (options.configGroups) === 'undefined') {
+    // most likely old config, must wrap it in config group
+    // deep clone to prevent circular structure
+    options = {
+      configGroups: [cloneDeep(options)]
+    };
+  }
 
-	// split the configs up if necessary
-	options.configGroups = exports.splitConfig(options.configGroups);
-	// place fields which do not belong in a group outside of configGroups
-	exports.factorize(options);
+  // split the configs up if necessary
+  options.configGroups = exports.splitConfig(options.configGroups);
+  // place fields which do not belong in a group outside of configGroups
+  exports.factorize(options);
 
-	// should we factorize common field values and place in defaultConfig? I don't think so
-	return options;
+  // should we factorize common field values and place in defaultConfig? I don't think so
+  return options;
 }
 
 /**
@@ -34,112 +34,112 @@ exports.makeCompatible = function(options) {
  * @param configGroups Array of configs
  * @return split array of configs
  */
-exports.splitConfig = function(configGroups) {
-	var res = [];
+exports.splitConfig = function (configGroups) {
+  var res = [];
 
-	configGroups.forEach(function(group, outer) {
-		if(typeof (group.targetXPath)!== 'undefined') {
-			// everything revolves around an xpath
-			if(Array.isArray(group.targetXPath)) {
-				// need to ensure it's array and not string so that code doesnt mistakenly separate chars
-				let renderToPathIsArray = Array.isArray(group.renderToPath);
+  configGroups.forEach(function (group, outer) {
+    if (typeof (group.targetXPath) !== 'undefined') {
+      // everything revolves around an xpath
+      if (Array.isArray(group.targetXPath)) {
+        // need to ensure it's array and not string so that code doesnt mistakenly separate chars
+        let renderToPathIsArray = Array.isArray(group.renderToPath);
 
-				// group up custom classes according to index
-				let groupedCustomClasses = [];
-				if(group.customClasses) {
-					group.customClasses.forEach(function(customClass) {
-						if(typeof (customClass.targetXPathIndex) === 'number') {
-							if(typeof (groupedCustomClasses[customClass.targetXPathIndex]) === 'undefined') {
-								groupedCustomClasses[customClass.targetXPathIndex] = [customClass];
-							} else {
-								groupedCustomClasses[customClass.targetXPathIndex].push(customClass);
-							}
-							delete customClass.targetXPathIndex;
-						}
-					})
-				}
+        // group up custom classes according to index
+        let groupedCustomClasses = [];
+        if (group.customClasses) {
+          group.customClasses.forEach(function (customClass) {
+            if (typeof (customClass.targetXPathIndex) === 'number') {
+              if (typeof (groupedCustomClasses[customClass.targetXPathIndex]) === 'undefined') {
+                groupedCustomClasses[customClass.targetXPathIndex] = [customClass];
+              } else {
+                groupedCustomClasses[customClass.targetXPathIndex].push(customClass);
+              }
+              delete customClass.targetXPathIndex;
+            }
+          })
+        }
 
-				// a group should revolve around targetXPath
-				// break up the array, starting from the first element
-				group.targetXPath.forEach(function(xpath, inner) {
-					// deep clone as config may have nested objects
-					var config = cloneDeep(group);
+        // a group should revolve around targetXPath
+        // break up the array, starting from the first element
+        group.targetXPath.forEach(function (xpath, inner) {
+          // deep clone as config may have nested objects
+          var config = cloneDeep(group);
 
-					// overwrite targetXPath
-					config.targetXPath = xpath;
+          // overwrite targetXPath
+          config.targetXPath = xpath;
 
-					// sync up renderToPath array
-					if(renderToPathIsArray && typeof (group.renderToPath[inner]) !== 'undefined') {
-						config.renderToPath = group.renderToPath[inner] ? group.renderToPath[inner] : null;
-					} else {
-						// nullify
-						config.renderToPath = null;
-					}
+          // sync up renderToPath array
+          if (renderToPathIsArray && typeof (group.renderToPath[inner]) !== 'undefined') {
+            config.renderToPath = group.renderToPath[inner] ? group.renderToPath[inner] : null;
+          } else {
+            // nullify
+            config.renderToPath = null;
+          }
 
-					// sync up relatedElementActions array
-					if(group.relatedElementActions && 
-						typeof (group.relatedElementActions[inner]) !== 'undefined' && 
-						Array.isArray(group.relatedElementActions[inner])) {
-						config.relatedElementActions = group.relatedElementActions[inner];
-					}
+          // sync up relatedElementActions array
+          if (group.relatedElementActions &&
+            typeof (group.relatedElementActions[inner]) !== 'undefined' &&
+            Array.isArray(group.relatedElementActions[inner])) {
+            config.relatedElementActions = group.relatedElementActions[inner];
+          }
 
-					// sync up customClasses
-					if(typeof (groupedCustomClasses[inner]) !== 'undefined') {
-						config.customClasses = groupedCustomClasses[inner];
-					}
+          // sync up customClasses
+          if (typeof (groupedCustomClasses[inner]) !== 'undefined') {
+            config.customClasses = groupedCustomClasses[inner];
+          }
 
-					// duplicate ignoredPriceElements string / array if exists
-					if(group.ignoredPriceElements) {
-						config.ignoredPriceElements = group.ignoredPriceElements;
-					}
+          // duplicate ignoredPriceElements string / array if exists
+          if (group.ignoredPriceElements) {
+            config.ignoredPriceElements = group.ignoredPriceElements;
+          }
 
-					// that's all, append
-					res.push(config);
-				});
-			} else {
-				// must be a single string
-				res.push(group)
-			}
-    	}
-	});
-	return res;
+          // that's all, append
+          res.push(config);
+        });
+      } else {
+        // must be a single string
+        res.push(group)
+      }
+    }
+  });
+  return res;
 }
 
 /**
  * This is a helper function to factorize common fields of groups
  * and also place them outside configGroups in order to be compatible
- * with latest structure. 
+ * with latest structure.
  * TODO (MAYBE) : Factorize all fields with common values in configGroups
  * and place it as a property of the defaultConfig
  * @param options the sezzle config
  * @return no return. The function mutates the object
  */
-exports.factorize = function(options) {
-	const fieldsToFactorize = ["merchantID", "forcedShow", "altLightboxHTML", "apModalHTML", "qpModalHTML"];
-	var choose = {};
+exports.factorize = function (options) {
+  const fieldsToFactorize = ["merchantID", "forcedShow", "altLightboxHTML", "apModalHTML", "qpModalHTML"];
+  var choose = {};
 
-	// assumption is being made that all these fields are the same across all config groups
-	// it is a reasonable assumption to make as :
-	// - one config as a whole should only be assigned to one merchantID
-	// - forcedShow is only useful if the country in which the widget is served is not in the supported list
-	//   so it's reasonable to assume that forcedShow should be the same value for all configs
-	// - as the widget only supports one modal currently, there is no capability of loading multiple modals
+  // assumption is being made that all these fields are the same across all config groups
+  // it is a reasonable assumption to make as :
+  // - one config as a whole should only be assigned to one merchantID
+  // - forcedShow is only useful if the country in which the widget is served is not in the supported list
+  //   so it's reasonable to assume that forcedShow should be the same value for all configs
+  // - as the widget only supports one modal currently, there is no capability of loading multiple modals
 
-	options.configGroups.forEach(function(group) {
-		fieldsToFactorize.forEach(function(field) {
-			if(group[field] !== undefined) {
-				choose[field] = group[field];
-				delete group[field]; 
-			}
-		});
-	});
+  options.configGroups.forEach(function (group) {
+    fieldsToFactorize.forEach(function (field) {
+      if (group[field] !== undefined) {
+        choose[field] = group[field];
+        delete group[field];
+      }
+    });
+  });
 
-	// insert to outermost layer
-	for(var key in choose) {
-		options[key] = choose[key];
-	}
+  // insert to outermost layer
+  for (var key in choose) {
+    options[key] = choose[key];
+  }
 
-	return;
+  return;
 }
 
 /**
@@ -156,9 +156,9 @@ exports.isNumeric = function (n) {
  * @param xpath string Ex: './.class1/#id'
  * @returns string[] Ex: ['.', '.class', '#id']
  */
-exports.breakXPath = function(xpath) {
+exports.breakXPath = function (xpath) {
   return xpath.split('/')
-    .filter(function(subpath) {
+    .filter(function (subpath) {
       return subpath !== ''
     });
 }
@@ -207,7 +207,7 @@ exports.parsePrice = function (price) {
  * @param referenceNode Element to insert after
  */
 exports.insertAfter = function (el, referenceNode) {
-	referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
+  referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
 }
 
 /**
@@ -216,9 +216,9 @@ exports.insertAfter = function (el, referenceNode) {
  * @param referenceElement Element to grab parent element
  */
 exports.insertAsFirstChild = function (element, referenceElement) {
-	referenceElement.parentElement.insertBefore(element, referenceElement);
-	//bump up element above nodes which are not element nodes (if any)
-	while (element.previousSibling) {
-		element.parentElement.insertBefore(element, element.previousSibling);
-	}
+  referenceElement.parentElement.insertBefore(element, referenceElement);
+  //bump up element above nodes which are not element nodes (if any)
+  while (element.previousSibling) {
+    element.parentElement.insertBefore(element, element.previousSibling);
+  }
 }
