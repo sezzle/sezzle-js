a36ff116cab794413e95f792adc1a45a90174016
hardik.nagda <hardik.nagda@sezzle.com>
2020-03-16 17:46:59 +0530

    code-refactor

---
+++ src/refactoredCode/sezzle-es6.js
@@ -0,0 +1,43 @@
+import { constructConfig, state } from './sezzleWidgetState';
+import logEvent from './eventLogger';
+import utils from './utils';
+import {initWidget} from './coreFunctions';
+
+export default class SezzleJS {
+    constructor (options) {
+        constructConfig(options);
+    }
+    /**
+     * Initialise the widget if the
+     * country is supported or the widget
+     * is forced to be shown
+    */
+    async init () {
+        // no widget to render
+        if(!state.configGroups.length) return;
+        // if config has forcedShow set to true
+        if(state.forcedShow) {
+            logEvent('request');
+            utils.loadCSS();
+            utils.getCountryCodeFromIP(countryCode => {
+                // only inject Google tag manager for clients visiting from the United States or Canada
+                if (countryCode === 'US' || 'CA') {
+                  const win = window.frames.szl;
+                  if (win && !this.noGtm) {
+                    win.postMessage('initGTMScript', 'http://localhost:9001/');
+                    // win.postMessage('initGTMScript', 'https://tracking.sezzle.com');
+                  }
+                }
+              });
+        }
+        // if config has forcedShow set to false
+        else {
+            const countryCode = await utils.getCountryCodeFromIP();
+            if (state.supportedCountryCodes.includes(countryCode)) {
+                logEvent('request');
+                await utils.loadCSS();
+                initWidget();
+            };
+        };
+    }   
+}
